27bf59792c8b931fbe06a228bcb3e23e04b575c4
Mike Rossetti <s2t2@users.noreply.github.com>
2020-05-05 14:06:58 -0400

    More Polo properties and methods

--- my_lambdata/polos.py
+++ my_lambdata/polos.py
@@ -9,10 +9,15 @@ class Polo():
         self.size = size
         self.color = color
 
-    def wash(self):
-        print("WASHING THE POLO!")
+    @property
+    def full_name(self):
+        return f"{self.size} {self.color}"
 
-    def fold(self):
+    def wash(self):
+        print(f"WASHING THE {self.size} {self.color} POLO!")
+
+    @staticmethod
+    def fold():
         print("FOLDING THE POLO!")
 
 if __name__ == "__main__":
@@ -24,8 +29,10 @@ if __name__ == "__main__":
 
     p1 = Polo(size="Small", color="Blue")
     print(p1.size, p1.color)
+    print(p1.full_name)
     p1.wash()
 
     p2 = Polo(size="Large", color="Yellow")
     print(p2.size, p2.color)
+    print(p2.full_name)
     p2.fold()
