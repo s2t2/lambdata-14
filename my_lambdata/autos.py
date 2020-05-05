aa53d56c05f6412c8e5e94db3399a747c145cd78
Mike Rossetti <s2t2@users.noreply.github.com>
2020-05-05 14:37:46 -0400

    Remembering to print the attributes

--- my_lambdata/autos.py
+++ my_lambdata/autos.py
@@ -2,6 +2,15 @@
 
 class Auto():
     def __init__(self, make, model, year, color, num_wheels):
+        """
+        Params:
+            make str ...
+            model str ...
+            year int ...
+            color str ...
+            num_wheels int  ...
+        """
+
         self.make = make
         self.model = model
         self.year = year
@@ -14,39 +23,29 @@ class Auto():
     def advertise(self):
         print("BUY OUR", self.model)
 
-class Truck():
+class Truck(Auto):
     def __init__(self, make, model, year, color, num_wheels, bed_size):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.color = color
-        self.num_wheels = num_wheels
+        super().__init__(make, model, year, color, num_wheels)
         self.bed_size = bed_size
-
-    def drive(self):
-        print("WE ARE DRIVING", self.model)
-
-    def advertise(self):
-        print("BUY OUR", self.model)
 
 if __name__ == "__main__":
 
     car = Auto("Toyota", "Prius", 2020, "Blue", 4)
-    car.make
-    car.model
+    print(car.make)
+    print(car.model)
     car.drive()
     car.advertise()
 
     car2 = Auto("Tesla", "Model S", 2020, "Blue", 4)
-    car2.make
-    car2.year
+    print(car2.make)
+    print(car2.year)
     car2.drive()
     car2.advertise()
 
     truck = Truck("Tesla", "Cybertruck", 2020, "Silver", 8, "4x6")
-    truck.make
-    truck.model
-    truck.year
-    truck.bed_size
+    print(truck.make)
+    print(truck.model)
+    print(truck.year)
+    print(truck.bed_size)
     truck.drive()
     truck.advertise()
