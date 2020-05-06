e62fff8931e0d067bffdc0c0b5f176340940f5aa
Mike Rossetti <s2t2@users.noreply.github.com>
2020-05-06 13:03:37 -0400

    Refactor using oop

--- my_lambdata/assignment1.py
+++ my_lambdata/assignment1.py
@@ -3,39 +3,51 @@
 
 from pandas import DataFrame
 
-def add_state_names(my_df):
+class Wrangler():
     """
-    Converts a dataframe with a column of state abbreviations, adding a corresponding column of state names.
-
-    Params:
-        my_df a pandas.DataFrame with a column called "abbrev".
-
-    Example:
-        add_state_names(DataFrame({"abbrev": ["CA", "CO", "CT", "DC", "TX"]}))
-
-    Returns: a pandas.DataFrame with the original col as well as a "name" column.
+    Wrangler takes a dataframe and manipulates it.
     """
 
-    new_frame = my_df.copy()
+    def __init__(self, my_df):
+        """
+        Param: my_df a pandas.DataFrame with a column called "abbrev"
 
-    # need a list or dict with the abbrev/name mappings
-    names_map = {"CA": "Cali", "CO": "Colo",
-                 "CT": "Conn", "DC": "District of Columbia"}
+        Example: Wranger(df=DataFrame({"abbrev": ["CA", "CO", "CT", "DC", "TX"]}))
+        """
+        self.df = my_df
 
-    # create a new column which maps the existing column using our names map
-    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
-    # breakpoint()
-    # type(type(new_frame["abbrev"])) #> <class 'pandas.core.series.Series'>
-    # dir(new_frame["abbrev"])
-    new_frame["name"] = new_frame["abbrev"].map(names_map)
+    def add_state_names(self):
+        """
+        Converts a dataframe with a column of state abbreviations, adding a corresponding column of state names.
 
-    return new_frame
+        Returns: a pandas.DataFrame with the original col as well as a "name" column.
+
+        H/T: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+        """
+        names_map = {"CA": "Cali", "CO": "Colo",
+                    "CT": "Conn", "DC": "District of Columbia"}
+
+        self.df["name"] = self.df["abbrev"].map(names_map)
+
+    def inspect_columns(self):
+        print(self.df.columns)
+
 
 
 if __name__ == "__main__":
 
+    # df = DataFrame({"abbrev": ["CA", "CO", "CT", "DC", "TX"]})
+    # print(df.head())
+    # df2 = add_state_names(df)
+    # print(df2.head())
+
     df = DataFrame({"abbrev": ["CA", "CO", "CT", "DC", "TX"]})
     print(df.head())
 
-    df2 = add_state_names(df)
-    print(df2.head())
+    wrangler = Wrangler(df)
+    print(type(wrangler))
+    wrangler.inspect_columns()
+    #df2 = wrangler.add_state_names()
+    #print(df2.head())
+    wrangler.add_state_names()
+    print(wrangler.df.head())
