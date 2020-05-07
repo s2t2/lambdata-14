f35da3bdcb817bc32678744dd99c2b9e80dc7251
Mike Rossetti <s2t2@users.noreply.github.com>
2020-05-07 13:35:13 -0400

    Pytest

---
+++ test/strings2_test.py
@@ -0,0 +1,23 @@
+
+import pytest
+
+def test_upper():
+    #self.assertEqual("foo".upper(), "FOO")
+    assert "foo".upper() == "FOO"
+
+def test_isupper():
+    #self.assertTrue("FOO".isupper())
+    #self.assertFalse("Foo".isupper())
+    assert "FOO".isupper()
+    assert "Foo".isupper() == False
+
+def test_split():
+    s = "hello world"
+    #self.assertEqual(s.split(), ["hello", "world"])
+    assert s.split() == ["hello", "world"]
+
+    ## check that s.split fails when the separator is not a string
+    #with self.assertRaises(TypeError):
+    #    s.split(2)
+    with pytest.raises(TypeError):
+        s.split(2)
